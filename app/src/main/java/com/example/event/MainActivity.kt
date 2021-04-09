378bda4891e6347642ecbb0f8b6ed24d168dfd46
“Mett-Barrgit config --global user.name “Mett-Barr <ohmylove347@gmail.comgit config --global user.name Mett-Barr>
2021-04-10 03:04:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/event/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.event
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.event.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
